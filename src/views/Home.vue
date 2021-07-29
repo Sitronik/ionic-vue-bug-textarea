327e568a4b656f609e1831ffe15b2cb48d2a873c
Denis <dgrushak@gmail.com>
2021-07-29 17:17:21 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,25 +5,37 @@
         <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+
+<!--      <div id="container">-->
+      <div style="margin-top: 50px">
+        <ion-item>
+          <ion-textarea
+                  ref="target"
+                  v-show="show"
+                  :autoGrow="true"
+                  v-model="data"
+                  inputmode="text"
+                  :rows="1"
+                  @ionBlur="onBlur"
+          ></ion-textarea>
+            <span v-show="!show" @click="show = true">{{ data }}</span>
+        </ion-item>
       </div>
+<!--      </div>-->
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonTextarea } from '@ionic/vue';
+import { defineComponent, ref, watch } from 'vue';
 
 export default defineComponent({
   name: 'Home',
@@ -32,7 +44,27 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonItem,
+    IonTextarea
+  },
+  setup() {
+    const data = ref('Test value');
+    const show = ref(false);
+    const target= ref();
+
+    function onBlur() {
+      show.value = false;
+    }
+
+    watch(show, () => {
+      setTimeout(() => {
+        show.value ? target.value.$el.children[0].children[0].focus() : '';
+      }, 50);
+
+    });
+
+    return {data, show, onBlur, target};
   }
 });
 </script>
@@ -40,7 +72,7 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,13 +88,28 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
+}
+
+.sc-ion-textarea-md-h {
+  font-family: unset;
+  margin-top: 0px;
+  --padding-top: 0px;
+  --padding-bottom: 0px;
+  --padding-start: 0px;
+}
+
+.sc-ion-textarea-ios-h {
+  font-family: unset;
+  --padding-top: 0px;
+  --padding-bottom: 0px;
+  --padding-end: 0px;
 }
 </style>
